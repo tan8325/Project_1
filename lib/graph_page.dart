e445b0a5e12152c4acf6bc0949e1f140a68bf85d
tan8325 <tbach4@student.gsu.edu>
2025-03-26 13:15:46 -0400

    Added bottom navigation and separate lib files for each screen

---
+++ lib/graph_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GraphPage extends StatelessWidget {
+  const GraphPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Graph',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
