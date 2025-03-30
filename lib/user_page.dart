a8e6ffea3137f6e64b056fc674958b8f737fa1e6
Laiza <llaizanicole@gmail.com>
2025-03-29 22:05:53 -0400

    Adds circular progress indicator to user page

--- lib/user_page.dart
+++ lib/user_page.dart
@@ -5,10 +5,57 @@ class UserPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'User',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Circular Progress Indicator
+            SizedBox(
+              child: Stack(
+                children: [
+                  Center(
+                    child: Container(
+                      alignment: Alignment.topCenter,
+                      child: Transform.scale(
+                        // increases size of the progress indicator
+                        scale: 4,
+                        child: CircularProgressIndicator(
+                          value: 0.8, // placeholder value
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                            Colors.deepPurpleAccent,
+                          ),
+                          backgroundColor: Colors.grey,
+                          strokeWidth: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Container(
+                      alignment: Alignment.topCenter,
+                      width: 48,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 2,
+                            offset: Offset(0, 0),
+                          ),
+                        ],
+                      ),
+                      child: Text('\$1,600'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
